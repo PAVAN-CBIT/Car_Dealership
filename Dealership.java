4ca8cb00c0ee32e6f319078ddf3d2ed44b422d6b
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-11 17:27:25 +0530

    Update Dealership.java

--- Dealership.java
+++ Dealership.java
@@ -5,8 +5,11 @@ public static void main(String[] args)
 Customer cust1 = new Customer();
   cust1.setName("tom");
   cust1.setAdress("123 avenue");
-  cust1.setCashOnHand("12000");
+  cust1.setCashOnHand(12000);
+  Vehicle vehicle = new Vehicle;
+  Employee emp = new Employee;
   cust1.purchaseCar(vehicle,emp,false);
+
 
 
 
