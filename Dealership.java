2122545b10d118e70849ac73962ad4101ec6a0a6
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-11 18:09:29 +0530

    Update Dealership.java

--- Dealership.java
+++ Dealership.java
@@ -6,9 +6,15 @@ Customer cust1 = new Customer();
   cust1.setName("tom");
   cust1.setAdress("123 avenue");
   cust1.setCashOnHand(12000);
-  Vehicle vehicle = new Vehicle("Honda","Accord",15000);
-  Employee emp = new Employee;
+  Vehicle vehicle = new Vehicle("BMW","M3",20000);
+  Employee emp = new Employee();
   cust1.purchaseCar(vehicle,emp,false);
+  
+  Vehicle car = new Vehicle("BMW","M3",20000);
+
+ boolean value = car.equals(vehicle);
+  System.out.println(value);
+  
 
 
 
