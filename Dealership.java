ac0dcb8146cacf33f99909a823fa972816601578
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-11 17:17:21 +0530

    Update Dealership.java

--- Dealership.java
+++ Dealership.java
@@ -1,1 +1,24 @@
+public class Dealership
+{
+public static void main(String[] args)
+{
+Customer cust1 = new Customer();
+  cust1.setName("tom");
+  cust1.setAdress("123 avenue");
+  cust1.setCashOnHand("12000");
+  cust1.purchaseCar(vehicle,emp,false);
 
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
