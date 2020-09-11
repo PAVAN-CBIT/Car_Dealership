f28b8918580296890a539aa4eb54b01000ef64af
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-11 17:11:02 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,41 @@
+public class Customer
+{
+  private String name;
+  private String address;
+  private double cashOnHand;
+  
+  public String getName()
+  {
+     return name;
+  }
+  public void setName(String name)
+  {
+  this.name = name;
+  }
+  public String getAdress()
+  {
+     return adress;
+  }
+  public void setAdress(String adress)
+  {
+  this.adress = adress;
+  }
+  public double  getCashOnHand()
+  {
+     return CashonHand;
+  }
+  public void setCashOnHand(double cashonHand)
+  {
+    this.cashonHand=cashonHand;
+  
+  }
+ public void purchaseCar(Vehicle vehicle ,Employee emp,boolean finance)
+ {
+ 
+ }
 
+
+
+
+
+}
